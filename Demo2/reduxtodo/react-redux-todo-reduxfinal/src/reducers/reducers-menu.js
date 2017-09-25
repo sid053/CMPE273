6d132c68eab71f20883e07ae5adec68c6b00f478
siddharth suthar <sid@siddharths-MacBook-Pro.local>
2017-09-25 13:25:10 -0700

    Added menu file

---
+++ Demo2/reduxtodo/react-redux-todo-reduxfinal/src/reducers/reducers-menu.js
@@ -0,0 +1,26 @@
+
+export default function () {
+    return [
+        {
+            id: 1,
+            name: "Pasta",
+            price: 10,
+            description: "Its an Italian Cuisine"
+            
+        },
+         {
+            id: 2,
+            name: "Tandoori Chicken",
+            price: 20,
+            description: "Its an Indian Cuisine"
+            
+        },
+        {
+            id: 3,
+            name: "Fried Rice",
+            price: 12,
+            description: "Its Chinise Cuisine"
+            
+        }
+    ]
+}
